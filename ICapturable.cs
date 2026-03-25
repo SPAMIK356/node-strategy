45a3fa8140d95e4a9ed50c32786c392a09e4436d
spamik <maximbondarenko20@gmail.com>
2026-03-25 19:35:39 +0200

    Added interface for capturing

---
+++ ICapturable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NodeStrategy
+{
+    internal interface ICapturable
+    {
+
+        public void Capture(int by);
+
+    }
+}
