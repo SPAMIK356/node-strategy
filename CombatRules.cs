13bf889730b605357f8ff1b138afb63f86d1cf5f
spamik <maximbondarenko20@gmail.com>
2026-03-30 10:52:32 +0300

    Added static rules class

---
+++ CombatRules.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NodeStrategy
+{
+    public static class CombatRules
+    {
+
+        public static float combatLetality { get; private set; } = 0.15f;
+
+    }
+}
