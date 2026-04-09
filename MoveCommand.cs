5fda1ad39b2be259280eec97544be5b0e3e3e59f
spamik <maximbondarenko20@gmail.com>
2026-04-09 15:42:16 +0300

    Finished command logic

--- MoveCommand.cs
+++ MoveCommand.cs
@@ -7,11 +7,11 @@ namespace NodeStrategy
     class MoveCommand : Command
     {
         private Army army;
-        private MapElement targetElement;
+        private Node targetElement;
         private float progress = 0;
-        private const float progressGoal = 100f;
+        private const float progressGoal = 1f;
         public override string Name { get; protected set; }
-        public MoveCommand(Army army, MapElement target)
+        public MoveCommand(Army army, Node target)
         {
             Name = $"Переміщення {army.name} до {target.Name}";
             this.army = army;
@@ -19,15 +19,42 @@ namespace NodeStrategy
         }
         public override void Execute()
         {
-            if (progress == 0)
-            {
+            Edge currentPos = army.currentPosition as Edge;
 
-            }
+            progress += currentPos.traverseCost;
+
+            if(progress >= progressGoal) finishedExectuing = true;
         }
 
         public override bool IsValid()
         {
             throw new NotImplementedException();
+        }
+
+        public override void OnStart()
+        {
+            Node sourceNode = army.currentPosition as Node;
+
+
+            Edge? edge = sourceNode.GetConnection(targetElement);
+
+            if(edge == null)
+            {
+                throw new Exception($"Вершина {sourceNode.name} не з'єднана з {targetElement.name}");
+            }
+
+            sourceNode.TryRemoveArmy(army);
+
+
+            edge.AcceptArmy(army);
+
+        }
+
+        public override void OnFinish()
+        {
+            army.currentPosition.TryRemoveArmy(army);
+
+            targetElement.AcceptArmy(army);
         }
     }
 }
