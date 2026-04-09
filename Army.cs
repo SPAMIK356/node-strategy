355546e30f040b6b8c078cd7a61abdc1c0ef06fd
spamik <maximbondarenko20@gmail.com>
2026-04-09 15:50:22 +0300

    Ашчув зкщзукен игп

--- Army.cs
+++ Army.cs
@@ -15,7 +15,7 @@ namespace NodeStrategy
         public float traverseProgress { get; private set; }
         public int controledBy { get; private set; }
         public MapElement currentPosition { get; private set; }
-        public bool isDead { get => units > 0; }
+        public bool isDead { get => units <= 0; }
         public Army(int units, float exp, float expCap,int unitCap, int controledBy, MapElement currentPosition, string name)
         {
             this.units = unitCap < units ? unitCap : units;
