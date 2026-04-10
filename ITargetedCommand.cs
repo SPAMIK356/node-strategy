380ea84cd46f4d1b56dba50b93a707a746f07e02
spamik <maximbondarenko20@gmail.com>
2026-04-10 13:53:15 +0300

    Added interface

---
+++ ITargetedCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NodeStrategy
+{
+    interface ITargetedCommand
+    {
+        public int subjectId { get; }
+
+    }
+}
