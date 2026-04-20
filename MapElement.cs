0e9d4c1a9dcb7219a3dbb6dc9f843b6bd218447f
spamik <maximbondarenko20@gmail.com>
2026-04-20 10:53:14 +0300

    Added wrapper functions for components

--- MapElement.cs
+++ MapElement.cs
@@ -6,8 +6,17 @@ namespace NodeStrategy
 {
     abstract class MapElement
     {
+        public virtual bool AddComponent(Component component)
+        {
+            components.Add(component);
+            return true;
+        }
+        public virtual T? GetComponent<T>() where T : Component
+        {
+            return components.OfType<T>().FirstOrDefault();
+        }
         public string Name { get; protected set; }
-        public List<Component> components;
+        protected List<Component> components;
         public int id;
         public virtual int controledBy { get; protected set; }
         public MapElement(string name, int id)
