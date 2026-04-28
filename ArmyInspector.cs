936942c349a79c8108e8e8bf128637b4a6e27314
spamik <maximbondarenko20@gmail.com>
2026-04-28 20:49:18 +0300

    Finsiehd army description

--- ArmyInspector.cs
+++ ArmyInspector.cs
@@ -15,13 +15,37 @@ namespace NodeStrategy
             InitializeComponent();
         }
 
-        public void DisplayInfo(Army army)
+        public void DisplayInfo(Army army, Command asociatedCommand)
         {
             armyName.Text = army.name;
 
             string description = $"контролюються фракцією {army.id}" +
                 $"Юніти: {army.units}/{army.unitCap}\n" +
-                $"Рівень досвіду: {army.exp}/{army.expCap}";
+                $"Рівень досвіду: {army.exp}/{army.expCap}\n";
+
+            if(army.currentPosition is Node node)  
+            {
+                if (node.isContested)
+                {
+                    description += $"Б'ється у {node.Name}";
+                }
+                else
+                {
+                    description += $"Знаходиться у: {node.Name}";
+                }
+            }
+            else if(army.currentPosition is Edge edge){
+                if(asociatedCommand == null)
+                {
+                    throw new Exception("Армія знаходиться на ребрі без команди!");
+
+                }
+                if (asociatedCommand.description == null || asociatedCommand.description == string.Empty)
+                    throw new Exception("Відсутній опис команди!");
+                description += asociatedCommand.description;
+            }
+            
+
         }
     }
 }
